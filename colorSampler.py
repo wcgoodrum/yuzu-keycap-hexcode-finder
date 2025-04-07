e102acf5701ba7d3ca7fec00405d65e67f0dd1a9
will goodrum <123277391+wcgoodrum@users.noreply.github.com>
2025-04-07 16:23:36 -0400

    refactored

--- colorSampler.py
+++ colorSampler.py
@@ -3,31 +3,24 @@ import csv
 import math
 from PIL import Image
 
-# relative path to look in
-path = "keyimages"
-# square to sample from
-uprLftCorner = [ 63, 39]
-lwrRgtCorner = [192, 80]
+IMAGEDIR = "keyimages"
+TOPLEFT = ( 63, 39)
+BTMRGHT = (192, 80)
 
-bgnX = uprLftCorner[0]
-endX = lwrRgtCorner[0]
-bgnY = uprLftCorner[1]
-endY = lwrRgtCorner[1]
-totalPixs = (endX-bgnX) * (endY-bgnY)
-
-record = open("colorvalues.csv", "wb")
-fList = os.listdir(path)
+print("Slurping images from local path: " + IMAGEDIR)
+totalPixs = (BTMRGHT[0] - TOPLEFT[0]) * (BTMRGHT[1] - TOPLEFT[1])
 with open("colorvalues.csv", "w", newline='') as record:
     writer = csv.writer(record)
     writer.writerow(["yuzu name", "hex value", "red", "green", "blue"])
     
+    fList = os.listdir(imageDir)
     for f in fList:
         print(f)
-        image = Image.open(path + "\\" + f, 'r').load()
+        image = Image.open(IMAGEDIR + "\\" + f, 'r').load()
         red = grn = blu = 0
-        for i in range(bgnX, endX):
-            for j in range(bgnY, endY):
-                pixel = image[i,j]
+        for x in range(TOPLEFT[0], BTMRGHT[0]):
+            for y in range(TOPLEFT[1], BTMRGHT[1]):
+                pixel = image[x, y]
                 red += pixel[0]**2
                 grn += pixel[1]**2
                 blu += pixel[2]**2
@@ -38,4 +31,4 @@ with open("colorvalues.csv", "w", newline='') as record:
         hexStr = '#' + hex(redAvr)[2:] + hex(grnAvr)[2:] + hex(bluAvr)[2:]
         writer.writerow([f.replace(".jpg",""), hexStr, str(redAvr), str(grnAvr), str(bluAvr)])
         
-print("done!")
+print("Done!")
